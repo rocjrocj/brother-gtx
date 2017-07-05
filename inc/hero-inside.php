4e9b6b98ac069bbe0cd0e6c79da7504920f4189a
rocjrocj <roc@rocjohnson.com>
2017-07-05 12:59:15 -0500

    Commit 9 before clean up.

--- inc/hero-inside.php
+++ inc/hero-inside.php
@@ -7,5 +7,5 @@
     <!--<img src="images/gtx-logo.png" alt="GTX Logo" width="158">-->
     <h1>The <strong>X</strong> factor for your garment printing business</h1>
     <img src="images/gtx-printer-hero.png" alt="The new Brother GTX Direct to garment printer" width="713" class="img-responsive center-block">
-    <div class="but-red"><a href=""><strong>Connect</strong> with a <strong>DTG Specialist</strong> <i class="glyphicon glyphicon-menu-right"></i></a></div>
+    <div class="but-red"><a href="region.php"><strong>Connect</strong> with a <strong>DTG Specialist</strong> <i class="glyphicon glyphicon-menu-right"></i></a></div>
 </section>
